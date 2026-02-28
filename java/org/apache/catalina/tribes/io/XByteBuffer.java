299de314c7722885f59aeb78d40e137f7f34dc1e
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2026-02-28 20:19:17 +0800

    fix: remediate 1 alert(s) (#109) in java/org/apache/catalina/tribes/io/XByteBuffer.java via gemini

--- java/org/apache/catalina/tribes/io/XByteBuffer.java
+++ java/org/apache/catalina/tribes/io/XByteBuffer.java
@@ -576,8 +576,7 @@ public class XByteBuffer implements Serializable {
         }
         if (data != null && length > 0) {
             InputStream  instream = new ByteArrayInputStream(data,offset,length);
-            ObjectInputStream stream = null;
-            stream = (cls.length>0)? new ReplicationStream(instream,cls):new ObjectInputStream(instream);
+            ObjectInputStream stream = new ReplicationStream(instream, cls);
             message = stream.readObject();
             instream.close();
             stream.close();
