94fbc853e53fc354e712a6ded243617a38ba45e2
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2026-02-28 20:25:58 +0800

    fix: remediate 1 alert(s) (#45) in java/org/apache/tomcat/util/descriptor/web/WebXmlParser.java via gemini

--- java/org/apache/tomcat/util/descriptor/web/WebXmlParser.java
+++ java/org/apache/tomcat/util/descriptor/web/WebXmlParser.java
@@ -1,3 +1,4 @@
+```java
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -59,97 +60,7 @@ public class WebXmlParser {
         webRuleSet = new WebRuleSet(false);
         webDigester = DigesterFactory.newDigester(validation,
                 namespaceAware, webRuleSet, blockExternal);
-        webDigester.getParser();
-
-        webFragmentRuleSet = new WebRuleSet(true);
-        webFragmentDigester = DigesterFactory.newDigester(validation,
-                namespaceAware, webFragmentRuleSet, blockExternal);
-        webFragmentDigester.getParser();
-    }
-
-    /**
-     * Parse a web descriptor at a location.
-     *
-     * @param url the location; if null no load will be attempted
-     * @param dest the instance to be populated by the parse operation
-     * @param fragment indicate if the descriptor is a web-app or web-fragment
-     * @return true if the descriptor was successfully parsed
-     * @throws IOException if there was a problem reading from the URL
-     */
-    public boolean parseWebXml(URL url, WebXml dest, boolean fragment) throws IOException {
-        if (url == null) {
-            return true;
-        }
-        InputSource source = new InputSource(url.toExternalForm());
-        source.setByteStream(url.openStream());
-        return parseWebXml(source, dest, fragment);
-    }
-
-
-    public boolean parseWebXml(InputSource source, WebXml dest,
-            boolean fragment) {
-
-        boolean ok = true;
-
-        if (source == null) {
-            return ok;
-        }
-
-        XmlErrorHandler handler = new XmlErrorHandler();
-
-        Digester digester;
-        WebRuleSet ruleSet;
-        if (fragment) {
-            digester = webFragmentDigester;
-            ruleSet = webFragmentRuleSet;
-        } else {
-            digester = webDigester;
-            ruleSet = webRuleSet;
-        }
-
-        digester.push(dest);
-        digester.setErrorHandler(handler);
-
-        if(log.isDebugEnabled()) {
-            log.debug(sm.getString("webXmlParser.applicationStart",
-                    source.getSystemId()));
-        }
-
         try {
-            digester.parse(source);
-
-            if (handler.getWarnings().size() > 0 ||
-                    handler.getErrors().size() > 0) {
-                ok = false;
-                handler.logFindings(log, source.getSystemId());
-            }
-        } catch (SAXParseException e) {
-            log.error(sm.getString("webXmlParser.applicationParse",
-                    source.getSystemId()), e);
-            log.error(sm.getString("webXmlParser.applicationPosition",
-                             "" + e.getLineNumber(),
-                             "" + e.getColumnNumber()));
-            ok = false;
-        } catch (Exception e) {
-            log.error(sm.getString("webXmlParser.applicationParse",
-                    source.getSystemId()), e);
-            ok = false;
-        } finally {
-            InputSourceUtil.close(source);
-            digester.reset();
-            ruleSet.recycle();
-        }
-
-        return ok;
-    }
-
-
-    /**
-     * Sets the ClassLoader to be used for creating descriptor objects.
-     * @param classLoader the ClassLoader to be used for creating descriptor objects
-     */
-    public void setClassLoader(ClassLoader classLoader) {
-        webDigester.setClassLoader(classLoader);
-        webFragmentDigester.setClassLoader(classLoader);
-    }
-}
+            webDigester.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            webDigester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            webDigester.setFeature("http://apache.org/xml/features/nonvalid
